ac422f2ded2ec7726fae3a0537842b77f4ff6ada
Jason Zhu <27667649+renqiz@users.noreply.github.com>
2018-08-29 18:16:02 +0800

    Chang build to use cmake

--- Cpp.cmake
+++ Cpp.cmake
@@ -1,5 +1,7 @@
 #
-# Copyright (c) 2018 Drive Foundation
+# MIT License
+#
+# Copyright (c) 2018 drvcoin
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -19,31 +21,29 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 #
+# =============================================================================
+#
 
-ifeq ($(ROOT),)
-$(error "Please define ROOT variable to point to the repository's root folder")
-endif
+macro(bd_use_pthread target)
+  set(THREADS_PREFER_PTHREAD_FLAG ON)
+  find_package(Threads REQUIRED)
+  target_link_libraries(${ARGV0} Threads::Threads)
+endmacro(bd_use_pthread)
 
-OUTDIR = $(ROOT)/out
-BINDIR = $(OUTDIR)/bin
-CFGDIR = $(OUTDIR)/cfg
-PKGDIR = $(OUTDIR)/package
+macro(bd_lib target name path)
+  if (NOT TARGET ${ARGV1})
+    add_library(${ARGV1} STATIC IMPORTED)
+    set_target_properties(${ARGV1} PROPERTIES IMPORTED_LOCATION ${ARGV2})
+  endif (NOT TARGET ${ARGV1})
+  target_link_libraries(${ARGV0} ${ARGV1})
+endmacro(bd_lib)
 
-ifneq ($(TARGETLIB),)
-TARGETNAME = $(TARGETLIB)
-TARGETFILENAME = $(TARGETNAME).a
-else
-ifneq ($(TARGETSO),)
-TARGETNAME = $(TARGETSO)
-TARGETFILENAME = $(TARGETNAME).so
-else
-TARGETNAME = $(TARGETEXE)
-TARGETFILENAME = $(TARGETNAME)
-endif
-endif
+macro(bd_sys_lib target name)
+  find_library(${ARGV1} ${ARGV1})
+  target_link_libraries(${ARGV0} ${ARGV1})
+endmacro(bd_sys_lib)
 
-OBJDIR = $(OUTDIR)/obj/$(TARGETNAME)
-INCDIR = $(OUTDIR)/inc
-LIBDIR = $(BLDDIR)/lib
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
 
-BUILD_ROOT = $(ROOT)/build
